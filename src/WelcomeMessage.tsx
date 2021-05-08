0efe714b91be0bc9a445a6753188caa78ef709f2
Manon Escoffier <escoffier.manon@gmail.com>
2021-05-08 15:57:17 -0400

    Fixed Welcome msg loading

--- src/WelcomeMessage.tsx
+++ src/WelcomeMessage.tsx
@@ -1,18 +1,33 @@
-import { Box, Typography } from '@material-ui/core';
-import React from 'react';
+import { Fade, Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { logInfo } from './utils/logging';
+
+export const WelcomeMessage: React.FC<{ text: string, masterTimeOut: number }> = (props) => {
+  const logger = "WelcomeMessage";
+  const fadingTime = props.masterTimeOut / 3;
+  const showingTime = props.masterTimeOut / 2;
+  // INFO: masterTimeOut is the time during which the WelcomeMessage will be shown (aka: allocated time). It will be divided as such:  
+  // (1) FadeIn time will be 1/4 of allocated time since it happens twice (in and out). (2) And showing time will be 1/2 time, so it shows long enough to view correctly
+  const [msgFadeIn, setMsgFadeIn] = useState<boolean>(false);
 
 
 
+  useEffect(() => {
+    if (props.text.length > 0) {
+      logInfo(logger, `Welcome Message Changed to ${props.text}. Changing Fade in to true.`);
+      // When message is changed
+      setMsgFadeIn(true); // show message
+      setTimeout(() => setMsgFadeIn(false), showingTime);
+    }
+  }, [props.text])
 
-
-
-export const WelcomeMessage : React.FC<{text:string}> = (props) => {
   return (
-    // <Box display="flex" justifyContent="center" alignItems="center">
-      <span className="Welcome-Msg">
+    <Fade in={msgFadeIn} timeout={fadingTime}>
+      <Typography variant="h2" className="Welcome-Msg">
         {props.text}
-      </span>
-    // </Box>
+      </Typography>
+
+    </Fade>
   )
 }
 
--- src/WelcomePageLoader.tsx
+++ src/WelcomePageLoader.tsx
@@ -15,114 +15,54 @@ const defaultMessagesToDisplayState: MessagesToDisplayOld = {
   // ThirdMessage: { shouldShow: false, message: string }
 }
 
-// Dictionary with position  1(n=message), 2(message) and while keeping a sleep just transition from one to the other (and to the next etc)
-// everytime set the current message
-
 interface MessagesToDisplay {
   [id: string]: string;
 }
 
-
-
-
+// const [msgToDisplay, setMsgToDisplay] = useState<MessagesToDisplay>(defaultMessagesToDisplayState);
+// const messagesToDisplay: MessagesToDisplay = {
+//   1: "Hi There !",
+//   2: "Let's see what we have in store"
+// }
 export const WelcomePageLoader: React.FC = () => {
-  // const [msgToDisplay, setMsgToDisplay] = useState<MessagesToDisplay>(defaultMessagesToDisplayState);
-  // const messagesToDisplay: MessagesToDisplay = {
-  //   1: "Hi There !",
-  //   2: "Let's see what we have in store"
-  // }
-  const logger = "WelcomePageLoader";
-  const messagesToDisplay: string[] = ["Hi There !", "Let's see what we have in store...", "Still Loading"]
 
+  const logger = "WelcomePageLoader";
+  const messagesToDisplay: string[] = ["Hi there !", "Let's see what we have in store ..."]
+  const timeOut = 4000;
 
   const [msgToDisplay, setMsgToDisplay] = useState<string>("");
-  // const [msgStyle, setMsgStyle] = useState<any>()
-  const [msgFadeIn, setMsgFadeIn] = useState<boolean>(false);
-
-  const useStyles = makeStyles(() =>
-    createStyles({
-      message: {
-        // transition: "visibility 0s, opacity 0.5s linear"
-        opacity: 0.5
-      }
-    }));
-
-  // const styles = {
-  //   Message: {
-  //     transition: "visibility 0s, opacity 0.5s linear"
-  //   }
-  // }
-
-  const showMessage = async (index: number) => {
-    // return new Promise(async (resolve) => {
-    // setMsgFadeIn(false)
-    // setTimeout(() => { setMsgFadeIn(!msgFadeIn) }, 1000);
-
-    // setTimeout(() => { setMsgToDisplay(messagesToDisplay[index]); }, 1000);
-    logInfo(logger, `Switching to message ${messagesToDisplay[index]}`);
-    // setTimeout(() => { setMsgFadeIn(!msgFadeIn) }, 1000);
-    // setMsgFadeIn(true);
-
-    setMsgToDisplay(messagesToDisplay[index]);
-    setMsgFadeIn(true);
-    await sleep(3000);
-    setMsgFadeIn(false);
-    // setTimeout(() => { setMsgFadeIn(false) }, 3000);
-    // })
-  }
-
-  function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  const [doneWithIntroMsg, setDoneWithIntroMsg] = useState<boolean>(false);
 
   useEffect(() => {
-    // SHOW FIRST MESSAGE
-    // setMsgToDisplay(messagesToDisplay[0]);
-    // setMsgFadeIn(true);
-    // setTimeout(() => { setMsgFadeIn(false) }, 3000);
-    // setTimeout(() => {
-    //   setMsgToDisplay(messagesToDisplay[1]);
-    //   setMsgFadeIn(true);
-    //   setTimeout(() => { setMsgFadeIn(false) }, 3000);
-    // }, 5000);
+    showMessages();
+  }, []) // Run Once, when component mounts for first time
 
-    // showMessage(0);
-    // Show second msg after 5 seconds
-    // for (let i = 1; i < Object.keys(messagesToDisplay).length; i++) {
-    //   setTimeout(() => {
-    //     showMessage(i);
-    //   }, 5000)
-    // }
-    // await showMessage(i);
-    setMsgToDisplay(messagesToDisplay[0])
-    // logInfo(logger, `SettingMessageFor index ${i}`)
-    setMsgFadeIn(true);
-    // await sleep(1000)
-    setTimeout(() => {
-      setMsgFadeIn(false)
-      logInfo(logger, "SetFadingFalse");
-      setMsgToDisplay(messagesToDisplay[1])
-      logInfo(logger, "SetMessage");
-      setMsgFadeIn(true);
-      setTimeout(() => {
-        setMsgFadeIn(false)
-      }, 2000)
-    }, 2000);
-    // setMsgFadeIn(false);
-    // showMessage(i);
-    // sleep(4000);
-
-
-  }, [])
+  const showMessages = () => {
+    let i = 0;
+    setMsgToDisplay(messagesToDisplay[i]);
+    i++;
+    // Using setInterval to properly loop through intro messages 
+    let interval = setInterval(() => {
+      logInfo(logger, `Displaying ${messagesToDisplay[i]} at Index ${i}`)
+      setMsgToDisplay(messagesToDisplay[i])
+      i++;
+      if (i === messagesToDisplay.length) {
+        clearInterval(interval);
+        // setDoneWithIntroMsg(true);
+      }
+    }, timeOut);
+    logInfo(logger, "Interval is Finished");
+  }
 
   logInfo(logger, "Rendering");
   return (
-    <Fade in={msgFadeIn} timeout={1000}>
-      <Typography variant="h2" className="Welcome-Msg-Container">
-        <WelcomeMessage text={msgToDisplay} />
-      </Typography>
-    </Fade>
-
+    <div className="Welcome-Msg-Container">
+      {doneWithIntroMsg ?
+        <h2>Welcome !</h2>
+        :
+        <WelcomeMessage text={msgToDisplay} masterTimeOut={timeOut} />
+      }
+    </div>
   )
 
 }
